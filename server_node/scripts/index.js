d7a3e6a6e292db0cd5b01952024ef0a1c0634935
Unknown <knldustbin@gmail.com>
2016-11-01 16:16:01 +0100

    migrated earlier modifications on new branch

--- server_node/scripts/index.js
+++ server_node/scripts/index.js
@@ -1,84 +1,95 @@
-import * as d3 from "d3";
-var canvas = d3.select("div")
-.append("svg")
-.attr("width",800)
-.attr("height",800)
-
-var grayBackground = canvas
-.append("rect")
-.attr("x", 0)
-.attr("y", 0)
-.attr("width", 800)
-.attr("height", 800)
-.attr("fill",d3.rgb(200, 210, 220))
-
-var jsonData = []
-var id = require('./App')
-var host = window.location.hostname
-var frames = 0
-var ws = new WebSocket('ws://' + host + ':8080'+"/?id="+id );
-      ws.onmessage = function (event) {
-        frames++
-      // console.log( event.data)
-        jsonData = JSON.parse(event.data)      
-      };
-
-var w = window.innerWidth;
-var h = window.innerHeight;
-
-var mousePosition = {x:250, y:250};
-document.addEventListener('mousemove', function(mouseMoveEvent){
-
-  if (mouseMoveEvent.pageX > 800 || mouseMoveEvent.pageX<0
-   || mouseMoveEvent.pageY<0 || mouseMoveEvent.pageY >800)
+window.onload = function()
 {
-  mousePosition.x = 400
-  mousePosition.y = 400
+    var canvas = document.getElementById("canvas");
+    if(!canvas)
+    {
+        alert("Impossible de récupérer le canvas");
+        return;
+    }
+
+    var context = canvas.getContext("2d");
+    if(!context)
+    {
+        alert("Impossible de récupérer le context");
+        return;
+    }
+
+    context.canvas.width  = window.innerWidth;
+    context.canvas.height = window.innerHeight;
+
+    var worldSize = {x: 2000,y: 2000};
+ 
+    var Snake = require('./Snake')
+    var snakes = []
+
+    var jsonData = []
+    var id = require('./App')
+    var host = window.location.hostname
+    var frames = 0
+    var ws = new WebSocket('ws://' + host + ':8080'+"/?id="+id );
+          ws.onmessage = function (event) {
+            frames++
+          // console.log( event.data)
+            jsonData = JSON.parse(event.data) 
+            for (var i = 0; i<jsonData.length; i++) {
+              var exists = false;
+              var j = 0;
+              while (!exists && j<snakes.length) {
+                if (snakes[j].is(jsonData[i])) {
+                  exists = true;
+                  snakes[j].l = jsonData[i].l;
+                  snakes[j].add(jsonData[i].x, jsonData[i].y);
+                }
+              }
+              if (!exists) {
+                snakes.push(new Snake(jsonData[i].id, jsonData[i].l, jsonData[i].x, jsonData[i].y))
+              }
+            }     
+          };
+
+    var mousePosition = {x:0, y:0};
+
+    document.addEventListener('mousemove', function(mouseMoveEvent){   
+        mousePosition.x = mouseMoveEvent.pageX;
+        mousePosition.y = mouseMoveEvent.pageY;
+    }, false);   
+    
+    var sendCommand = () =>
+    {
+    setTimeout(sendCommand,16.667)
+    //console.log(mousePosition)
+    var command = { "mouse" : mousePosition }
+    ws.send( JSON.stringify(command)  )
+    }
+    setTimeout(sendCommand,1000)
+    
+    
+    var countFPS = () =>
+    {
+      setTimeout(countFPS,1000)
+      console.log("fps : " +frames)
+      frames = 0
+      console.log(JSON.stringify(jsonData,1));
+    }
+    setTimeout(countFPS,1000)
+    
+    
+    var draw = () =>
+    {
+      context.clearRect(0, 0, canvas.width, canvas.height);
+      context.beginPath();
+
+      for (var i=0; i<snakes.length; i++) {
+        context.arc(snakes[i].x, snakes[i].y, snakes[i].l*10 /*the radius*/, 0, Math.PI*2);
+        context.strokeStyle = "#6D071A";
+        context.stroke();
+        context.fillStyle = "#A5260A";
+        context.fill();
+      }
+    
+    window.requestAnimationFrame(draw);
+    }
+    
+    window.requestAnimationFrame(draw);
+
 }
-else
-{
-  mousePosition.x = mouseMoveEvent.pageX;
-  mousePosition.y = mouseMoveEvent.pageY;
-}
-
-}, false);
-
-var sendCommand = () =>
-{
-setTimeout(sendCommand,16.667)
-//console.log(mousePosition)
-var command = { "mouse" : mousePosition }
-ws.send( JSON.stringify(command)  )
-}
-setTimeout(sendCommand,1000)
-
-
-var countFPS = () =>
-{
-  setTimeout(countFPS,1000)
-  console.log("fps : " +frames)
-  frames = 0
-  console.log(JSON.stringify(jsonData,1));
-}
-setTimeout(countFPS,1000)
-
-
-var draw = () =>
-{
-   canvas.selectAll("circle")
-        .data ([]).exit().remove()
-
-        canvas.selectAll("circle")
-        .data (jsonData)
-        .enter()
-        .append("circle")
-        .attr("cx", (e) => e.x)
-        .attr("cy", (e) => e.y)
-        .attr("r", (e)=> e.r)
-        .attr("fill", (e) => d3.rgb(e.rgb[0] , e.rgb[1],e.rgb[2])  )
-
-
-window.requestAnimationFrame(draw);
-} 
-
-window.requestAnimationFrame(draw);

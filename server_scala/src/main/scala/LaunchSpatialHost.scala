e9f5826b9e1d4e8bb013beded49508997da278f7
Thomas SIMON <thomas.simon.work@gmail.com>
2016-11-03 14:55:24 +0100

    Implemented 2 spatial actor with host transfert

--- server_scala/src/main/scala/LaunchSpatialHost.scala
+++ server_scala/src/main/scala/LaunchSpatialHost.scala
@@ -1,8 +1,10 @@
 import akka.actor.{ActorRef, ActorSystem, Props}
+import core.CoreMessage.SetProvider
 import core.{Provider, Server}
 import game.GameEvent.Tick
-import game.spatialHost.{ManagerSpatialHost, SpatialHost, OtherSpatial}
+import game.spatialHost.{ManagerSpatialHost, OtherSpatial, SpatialHost}
 import game.GameEvent.Vector
+
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
 
@@ -12,15 +14,18 @@ object LaunchSpatialHost extends App {
   val spatialA  = actorSystem.actorOf(Props(new SpatialHost(Vector(0,0),Vector(500,1000),1)), "spatialA")
   val spatialB  = actorSystem.actorOf(Props(new SpatialHost(Vector(500,0),Vector(500,1000),2)), "spatialB")
 
-  spatialA ! OtherSpatial(spatialB)
-  spatialB ! OtherSpatial(spatialA)
+  spatialA ! OtherSpatial(spatialB,"E")
+  spatialB ! OtherSpatial(spatialA,"W")
 
   var listHost = List[ActorRef]()
-  listHost  = spatialA :: listHost
   listHost  = spatialB :: listHost
+  listHost  = spatialA :: listHost
 
   val manager  = actorSystem.actorOf(Props(new ManagerSpatialHost(listHost)), "manager")
   val provider  = actorSystem.actorOf(Props(new Provider(manager)), "provider")
+
+  spatialA ! SetProvider(provider)
+  spatialB ! SetProvider(provider)
 
   val cancellableA  = actorSystem.scheduler.schedule( 1000 milliseconds , 33.3333 milliseconds, spatialA, Tick())
   val cancellableB  = actorSystem.scheduler.schedule( 1000 milliseconds , 33.3333 milliseconds, spatialB, Tick())

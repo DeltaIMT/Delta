6f3d2247ed4158205a549f593286b3888882e400
yosuke78 <vannasay.souvannavong@gmail.com>
2016-11-18 13:22:52 +0100

    add transfert cases

--- framework/src/main/scala/core/abstract/AbstractHost.scala
+++ framework/src/main/scala/core/abstract/AbstractHost.scala
@@ -1,9 +1,22 @@
 package core.`abstract`
 
 import akka.actor.Actor
+import core.CoreMessage.{Transfert, TransfertTo}
+import core.HostPool
 
-abstract class AbstractHost extends Actor {
+abstract class AbstractHost(val hostPool: HostPool) extends Actor {
+
+  var elements = collection.mutable.HashMap[String,Any]()
+
   override def receive: Receive = {
+    case TransfertTo(element, host) => {
+      host ! Transfert(element, elements(element))
+      elements -= element
+    }
+
+    case Transfert(id, element) => {
+      elements += id -> element
+    }
     case _ => {}
   }
 }

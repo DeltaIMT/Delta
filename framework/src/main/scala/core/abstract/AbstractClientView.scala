f8af8092db1c8ad1321c8ca6fdf467402ec1ff51
Thomas SIMON <thomas.simon.work@gmail.com>
2016-11-18 09:14:56 +0100

    Implemented ClientView without zone to list conversion

--- framework/src/main/scala/core/abstract/AbstractClientView.scala
+++ framework/src/main/scala/core/abstract/AbstractClientView.scala
@@ -1,9 +1,35 @@
 package core.`abstract`
 
-import akka.actor.Actor
+import akka.actor.{Actor, ActorRef}
 
-class AbstractClientView extends Actor {
-  override def receive: Receive = {
-    case _ => {}
+case class Notify(any : Any)
+case object UpdateClient
+case class Zone(x :Double, y: Double, w : Double, h : Double)
+
+
+abstract class AbstractClientView(hosts: IndexedSeq[ActorRef],client : ActorRef) extends Actor {
+
+  // USER JOB
+  def dataToViewZone() : List[Zone] = ???
+  def onNotify(any: Any) :Unit = ???
+  def fromListToClientMsg(list : List[Any])= ???
+
+  // TODO HERE CONVERTION FROM ZONE TO LIST OF ELEMENT CONTAINED IN THE ZONES BY ASKING HOSTS
+  def zonesToList(zones : List[Zone]) :  List[Any] = {
+
   }
+
+  override def receive: Receive = {
+    case x : Notify => {
+      onNotify(x.any)
+    }
+    case UpdateClient => {
+      var list =zonesToList(dataToViewZone())
+      client ! fromListToClientMsg(list)
+    }
+
+  }
+
+
+
 }

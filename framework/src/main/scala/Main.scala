a210df6f07e0e1f1275d6895e55cb907affbf970
Thomas SIMON <thomas.simon.work@gmail.com>
2016-11-18 10:04:15 +0100

    HostPool creation to contain information about hosts format

--- framework/src/main/scala/Main.scala
+++ framework/src/main/scala/Main.scala
@@ -1,6 +1,6 @@
 import akka.actor.{ActorSystem, Props}
 import core.script_filled.UserHost
-import core.{Provider, Websocket}
+import core.{HostPool, Provider, Websocket}
 
 
 
@@ -12,14 +12,14 @@ object Main extends App{
   val numberOfClient = 100
   val hostsGridWidth= 5
   val hostsGridHeight= 5
-
-
+  val hostWidth = 200
+  val hostHeight = 200
 
   val hosts =  0 until hostsGridWidth*hostsGridHeight map {i=> actorSystem.actorOf(Props(new UserHost()),"host"+i)}
-  val providers = 0 until numberOfClient map {i=>actorSystem.actorOf(Props(new Provider(hosts)),"provider_"+i)}
+  val hostPool = new HostPool(hostWidth,hostHeight,hostsGridWidth,hostsGridHeight, hosts)
+  val providers = 0 until numberOfClient map {i=>actorSystem.actorOf(Props(new Provider(hostPool)),"provider_"+i)}
   val websockets = 0 until numberOfClient map {i=>new Websocket(providers(i),initialPort+i)}
   println("framework working")
-
 
   println("framework shutdownn")
   actorSystem.terminate()

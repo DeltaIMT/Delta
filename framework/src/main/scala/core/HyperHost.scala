d2a62e1941186a9d02c5767a50412618a170a6cf
yosuke78 <vannasay.souvannavong@gmail.com>
2016-11-18 15:48:35 +0100

    implements hyperhost

--- framework/src/main/scala/core/HyperHost.scala
+++ framework/src/main/scala/core/HyperHost.scala
@@ -1,14 +1,46 @@
 package core
 
 import akka.actor.ActorRef
+import akka.pattern._
+import akka.util.Timeout
+import scala.concurrent.duration._
+import core.CoreMessage.{GetList, GetListFilter}
 import core.user_import.Element
 
-class HyperHost(val host : ActorRef)  {
+import scala.collection.mutable
+import scala.concurrent.Future
 
-  def foreachRemote() = ???
-  def foreach() = ???
-  def set() = ???
-  def getList() :List[Element] = ???
-  def getListFilter(f : Element => Boolean ) :List[Element] = ???
+class HyperHost(val host : ActorRef)  {
+  implicit val timeout = Timeout(1.second)
+
+  def set(id: String, element: Element) = {
+    host ! Set(id,element)
+  }
+
+  def getList() : mutable.HashMap[String,Element] = {
+    var list = mutable.HashMap[String,Element]()
+    val futureList = host ? GetList
+    futureList.map(_ => {
+      _ match {
+        case x: mutable.HashMap[String,Element] => {
+          list = x
+      }
+      }
+    })
+    list
+  }
+
+  def getListFilter(f : Element => Boolean ) : mutable.HashMap[String,Element] = {
+    var list = mutable.HashMap[String,Element]()
+    val futureList = host ? GetListFilter(f)
+    futureList.map(_ => {
+      _ match {
+        case x: mutable.HashMap[String,Element] => {
+          list = x
+        }
+      }
+    })
+    list
+  }
 
 }

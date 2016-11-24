0dfdf27bd696225625a4f83d4d459e511988d00e
Thomas SIMON <thomas.simon.work@gmail.com>
2016-11-24 09:07:52 +0100

    cleaned code and import

--- framework/src/main/scala/core/HyperHost.scala
+++ framework/src/main/scala/core/HyperHost.scala
@@ -1,16 +1,11 @@
 package core
 
 import akka.actor.ActorRef
-import akka.dispatch.Foreach
-import akka.pattern._
 import akka.util.Timeout
 
 import scala.concurrent.duration._
-import core.CoreMessage.{Foreach, GetList, GetListFilter}
+import core.CoreMessage.{Foreach}
 import core.user_import.Element
-
-import scala.collection.mutable
-import scala.concurrent.Future
 
 class HyperHost(val host : ActorRef)  {
   implicit val timeout = Timeout(1.second)
@@ -22,5 +17,4 @@ class HyperHost(val host : ActorRef)  {
   def foreach(f : (Element) =>  Unit){
     host ! Foreach(f)
   }
-
 }

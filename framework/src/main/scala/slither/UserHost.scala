a1cd1357eb630ef84761a9015fd170c9a5e34368
Thomas SIMON <thomas.simon.work@gmail.com>
2016-11-28 09:23:00 +0100

    beginning of a simple app

--- framework/src/main/scala/slither/UserHost.scala
+++
@@ -1,7 +0,0 @@
-
-class slither(x: Double, y : Double) extends Observable with Element(x,y)
-
-/*class myHost extends Host{
-  override def tick(): Unit = ??? k() = ???
-  override def onMsg(msg: String): Unit = ???(message: String) = ???
-}*/

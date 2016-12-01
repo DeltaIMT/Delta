056cea6d9a2bec8b16d055975a52b4f134a0e10e
Knluz <knldustbin@gmail.com>
2016-12-01 09:11:59 +0100

    removed useless ping tests

--- framework_node/scripts/providedCode.js
+++ framework_node/scripts/providedCode.js
@@ -41,67 +41,8 @@ setTimeout(sendCommand,1000)
 
 var target = 'http://localhost:5000'
 
-// using net-ping
-/*var pingTest1 = () =>
-{
-    var ping = require ("net-ping")
-
-    session.pingHost (target, function (error, target, sent, received) {
-        var ms = received - sent
-        if (error)
-            console.log (target + ": " + error.toString ())
-        else
-            console.log (target + ": Alive (ms=" + ms + ")")
-    })
-}*/
-// using ping-wrapper
-/*var pingTest2 = () =>
-{
-    var Ping = require('ping-wrapper')
-    Ping.configure()
-
-    var ping = new Ping(target)
-
-    ping.on('ping', function(data){
-        console.log('Ping %s: time: %d ms', data.host, data.time)
-    })
-
-    ping.on('fail', function(data){
-        console.log('Fail', data)
-    })
-
-    //ping.stop()
-}*/
-// using tcp-ping : net.Socket is not a constructor...
-var pingTest3 = () =>
-{
-    var port = 5000 //TODO
-    var tcpp = require('tcp-ping')
-
-    tcpp.probe(target, port, function(err, available) {
-        console.log(available)
-    })
-
-    tcpp.ping({ address: target }, function(err, data) {
-        console.log(data)
-    })
-}
-// using jjg-ping : cannot identify operating system...
-var pingTest4 = () =>
-{
-    var ping = require('jjg-ping')
-
-    ping.system.ping(target, function(latency, status) {
-        if (status) {
-            console.log('The target is reachable (' + latency + ' ms ping).')
-        }
-        else {
-            console.log('The target is unreachable.')
-        }
-    })
-}
 // using ping : seems to be an OS problem as well...
-var pingTest5 = () =>
+var pingTest = () =>
 {
     var ping = require('ping');
 
@@ -118,4 +59,4 @@ var pingTest5 = () =>
 module.exports.dataManipulation = (f) => { dataManipulationFunction = f }
 module.exports.commandToServer = (f) => { defineCommandToServer = f }
 module.exports.countFps = () => { return fps }
-module.exports.pingMeasurement = () => { return pingTest5() }
+module.exports.pingMeasurement = () => { return pingTest() }

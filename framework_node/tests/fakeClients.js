c170f1866f3f8c0f15b906214b3cff662d558a81
Thomas SIMON <thomas.simon.work@gmail.com>
2017-02-07 09:18:40 +0100

    Removing useless files, renaming others

--- framework_node/tests/fakeClients.js
+++ framework_node/tests/fakeClients.js
@@ -3,28 +3,23 @@ var host = '127.0.0.1'
 var uuid = require('node-uuid');
 var id_g = uuid.v4()
 var k = 0
-var N = 200
+var N = 20000
 var zlib = require('zlib');
 
 var createOne = () => {
-
     var k2 = k
     k++
     if (k < N) {
 
-        setTimeout(createOne, 150)
+        setTimeout(createOne, 100)
     }
-
-
     var id = id_g + k
     var address1 = 'ws://' + host + ':9000' + "/?id=" + id
-
     var wsPort = new WebSocket()
     console.log("Searching port at 9000")
     wsPort.connect(address1, null)
     wsPort.on('connect', function (connection) {
         console.log(k2 + " connected to 9000");
-
         connection.on('message', function (message) {
             if (message.type === 'utf8') {
                 console.log(k2 + " received: '" + message.utf8Data + "'");
@@ -35,77 +30,13 @@ var createOne = () => {
                 ws.connect(address2, null)
                 ws.on('connect', function (connection2) {
 
-                    var currentPos = { x: 1.0, y: 0 };
-
                     console.log(k2 + " connected to " + address2);
-
-                    connection2.on('message', function (message) {
-                        if (message.type === 'utf8') {
-                            //    console.log(k2 + " received: '" + message.utf8Data + "'");
-                            var data = zlib.gunzipSync(new Buffer(message.utf8Data,'base64'))    
-                            var obj = JSON.parse(data)
-                            obj.forEach(e => {
-                                if (e.cam != undefined) {
-
-                                    currentPos.x = e.cam.x
-                                    currentPos.y = e.cam.y
-                                    //          console.log("cam found :" + e.cam.x + " " + e.cam.y)
-                                }
-                            })
-
-
-                        }
-                    });
-
-                    var mouseSpeed = { x: 1.0, y: 0.0 }
-                    var mousePosition = { x: 0, y: 0 }
-                    var sendCommand = () => {
-
-                        mouseSpeed.x = (mouseSpeed.x * 0.9 + (Math.random() - 0.5) * 100)
-                        mouseSpeed.y = (mouseSpeed.y * 0.9 + (Math.random() - 0.5) * 100)
-                        mousePosition.x += mouseSpeed.x
-                        mousePosition.y += mouseSpeed.y
-
-                        if (mousePosition.x < 100) mouseSpeed.x =Math. abs(mouseSpeed.x)
-                        if (mousePosition.y < 100) mouseSpeed.y = Math.abs(mouseSpeed.y)
-                        if (mousePosition.x > 1000) mouseSpeed.x = -Math.abs(mouseSpeed.x)
-                        if (mousePosition.y > 1000) mouseSpeed.y = -Math.abs(mouseSpeed.y)
-
-
-                        var toServer
-                        if (currentPos.x != undefined)
-                            toServer = JSON.stringify([{ hosts: [[currentPos.x * 1.0, currentPos.y * 1.0]], data: JSON.stringify(mousePosition) }])
-                        else
-                            toServer = JSON.stringify([{ hosts: [[]], data: "" }])
-                        //    console.log(k2 + " Sending :\n" + toServer)
-
-                        setTimeout(sendCommand, 16.66)
-                        connection2.sendUTF(toServer)
-                    }
-                    setTimeout(sendCommand, 1000)
-
-
+                   connection2.close()
+                   connection.close()
 
                 })
-
             }
         });
     })
-
-
-
-    // wsPort.onmessage = function (event) {
-    //     data = event.data
-    // console.log("Connection to : "+'ws://' + host +':'+data +"/?id="+id)
-    //     ws = new WebSocket( )
-    //     ws.onmessage = function (event) {
-    //         frames++ 
-    //         dataManipulationFunction(event.data)
-    //     }
-    // }
-
-
 }
-
-
 createOne()

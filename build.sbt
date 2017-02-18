d275ffe84676d5da7452cfd5853a714a7ec81a6d
Thomas SIMON <thomas.simon.work@gmail.com>
2017-02-18 16:28:01 +0100

    added stgy without scalajs

--- build.sbt
+++ build.sbt
@@ -2,7 +2,7 @@ scalaVersion in ThisBuild := "2.11.8"
 
 scalacOptions in ThisBuild ++= Seq("-feature", "-language:postfixOps")
 
-lazy val root = project in file(".") aggregate(framework,stgy_scalajs_server,stgy_scalajs_client, paint_server, championship_server, splatoon_server)
+lazy val root = project in file(".") aggregate(framework,stgy_scalajs_server,stgy_scalajs_client,stgy_server, paint_server, championship_server, splatoon_server)
 
 
 lazy val framework = project in file("framework") settings(
@@ -16,6 +16,10 @@ lazy val stgy_scalajs_server = project in file("stgy_scalajs_server") settings(
 lazy val stgy_scalajs_client = project in file("stgy_scalajs_client") settings(
   version := "1.0.0"
 ) dependsOn(stgy_scalajs_server)
+
+lazy val stgy_server = project in file("stgy_server") settings(
+  version := "1.0.0"
+) dependsOn(framework)
 
 lazy val paint_server = project in file("paint_server") settings(
   version := "1.0.0"
---
+++ stgy_server/src/main/scala/stgy/StgyClientView.scala
@@ -0,0 +1,93 @@
+package stgy
+
+import akka.actor.ActorRef
+import akka.actor.FSM.->
+import core.clientView.{ClientView, ClientViewActor}
+import core.host.{HostObserver, HostPool}
+import core.spatial.Zone
+
+
+class StgyClientView(id :String) extends ClientView(id) {
+
+  val HP = HostPool[StgyHost, StgyHostObserver]
+  var pos = Vec(1500, 1500)
+  var xp = 0.0
+  var numberOfUnit = 1.0
+
+  var hashIdColor = collection.mutable.Map[String, Boolean]()
+  var hashIdChangeHost = collection.mutable.Map[String, Boolean]()
+  var hashTime = collection.mutable.Map[String, Int]()
+
+  override def dataToViewZone(): Zone = new SquareZone(pos.x - 1920/2, pos.y - 1080/2, 1920, 1080)
+
+  override def onNotify(any: Any): Unit = {
+
+    any match {
+      case _ => {
+       // println("notify not matched")
+      }
+    }
+  }
+
+  override def onDisconnect(any: Any): Unit = {
+    //  hostPool.getHyperHost(x, y).exec(l => l -= idBall)
+  }
+
+  override def fromListToClientMsg(list: List[Any]) = {
+
+    // hashTime uploaded each time "fromListToClientMsg is called :
+    // if the element is unknown, we added it inside hashTime, else we bring its value to 5
+    val unitys = list.filter(x => x.isInstanceOf[Unity]).asInstanceOf[List[Unity]]
+    unitys.foreach(u => {
+      if (!hashIdColor.contains(u.id))
+        hashIdColor += u.id -> false
+      else
+        hashIdColor(u.id) = true
+      // hashTime uploaded each time "fromListToClientMsg is called :
+      // if the element is unknown, we added it inside hashTime, else we bring its value to 5
+      if (!hashTime.contains(u.id))
+        hashTime += u.id -> 5
+      else {
+        if (!hashIdChangeHost.contains(u.id)) {
+          hashIdChangeHost += u.id -> true
+        }
+        else {
+          if (hashTime(u.id) == 4)
+            hashIdChangeHost(u.id) = false
+          else
+            hashIdChangeHost(u.id) = true
+        }
+        hashTime(u.id) = 5
+      }
+    }
+    )
+    // while processing "fromListToClientMsg", we decrease the value of each key we have
+    hashTime.keys.foreach(k => hashTime(k) = hashTime(k) - 1)
+
+
+    val listString = list.map {
+      case u: Unity => {
+        val colorString = if ((!hashIdColor(u.id)) || hashIdChangeHost(u.id)) s""","color":[${u.color.r},${u.color.g},${u.color.b}]""" else ""
+        u match {
+          case e: Commander => {
+            s"""{"type":"com","id":"${e.id}","spawning":"${1.0 - e.canSpawnIn / e.frameToSpawn.toFloat}","xp":"${e.xp}","mine":${id == e.clientId},"health":"${e.health/e.maxHealth}","x":"${e.x.toInt}","y":"${e.y.toInt}"${colorString}}"""
+          }
+          case e: Bowman => {
+            s"""{"type":"bowman","id":"${e.id}","mine":${id == e.clientId},"health":"${e.health/e.maxHealth}","xp":"${e.xp}","x":"${e.x.toInt}","y":"${e.y.toInt}"${colorString}}"""
+          }
+          case e: Swordman => {
+            s"""{"type":"swordman","id":"${e.id}","mine":${id == e.clientId},"health":"${e.health/e.maxHealth}","xp":"${e.xp}","x":"${e.x.toInt}","y":"${e.y.toInt}"${colorString}}"""
+          }
+          case e: Arrow => {
+            s"""{"type":"arrow","id":"${e.id}","x":"${e.x.toInt}","y":"${e.y.toInt}"}"""
+          }
+        }
+      }
+      case e => "NOT ELEMENT : " + e
+    } ++ List(
+      s"""{"type":"camera","id":"0","x":"${pos.x.toInt}","y":"${pos.y.toInt}"}""",
+      s"""{"type":"other","id":"1","n":"${numberOfUnit}","xp":"${xp}","usedXp":"${xp}"}""")
+    val string = listString.mkString("[", ",", "]")
+    Left(string)
+  }
+}

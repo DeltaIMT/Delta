b17a465f3816c01770826c69e7a78f3a22fdb5e7
Charly <charly.ouin@club-internet.fr>
2017-02-18 16:32:22 +0100

    add some comment to the framework code

--- framework/src/main/scala/core/clientView/ClientView.scala
+++ framework/src/main/scala/core/clientView/ClientView.scala
@@ -6,12 +6,16 @@ import core.spatial.Zone
 
 abstract class ClientView(val id : String) {
 
+  // define the area that the client must see
   def dataToViewZone(): Zone
 
+  // define the action which should occured when the ClientView received a notify from a Host
   def onNotify(any: Any): Unit
 
+  // define the action which should occured when the Client is disconnected
   def onDisconnect(any: Any): Unit
 
+  // convert all the data that should be sent to the client from a list into a message
   def fromListToClientMsg(list: List[Any]): Either[String,ByteBuffer]
 
 
--- framework/src/main/scala/core/spatial/Zone.scala
+++ framework/src/main/scala/core/spatial/Zone.scala
@@ -1,5 +1,5 @@
 package core.spatial
-
+// define an area, by defining 2 methods : if a viewable is inside a zone ; if there is a commun area between 2 zone.
 trait Zone {
 
   def contains(e : Viewable) : Boolean
